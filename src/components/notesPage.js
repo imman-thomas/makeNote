b42e145b38a11298d58faae0d2ac367be5d166f5
imman-thomas <immanthomas@gmail.com>
2019-04-05 11:57:15 +0800

    initial commit

---
+++ src/components/notesPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '../index.css'
+
+export class notesPage extends Component {
+  render() {
+    return (
+      <div>
+        <input   class="box"/><button >add</button>
+      </div>
+    )
+  }
+}
+
+export default notesPage
--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+import NotesPage from './components/notesPage'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+export class MakeNotes extends Component {
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default MakeNotes
+
+
+
+//------------------------------------------------------------------
+ReactDOM.render(
+
+    <NotesPage/>,
+    document.getElementById('root')
+
+)
+
