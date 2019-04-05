21f1acc753931dd1de5272ca8b5b40d1cd8c1a1d
imman-thomas <immanthomas@gmail.com>
2019-04-05 17:05:54 +0800

    Notes template for add del completed

--- src/components/notesPage.js
+++ src/components/notesPage.js
@@ -2,11 +2,70 @@ import React, { Component } from 'react'
 import '../index.css'
 
 export class notesPage extends Component {
+constructor(props){
+super(props)
+
+this.state={
+  cards:[],
+  count:0
+
+}
+
+this.c =[]
+}
+  
+
+AddButtonHandler=(e)=>{
+  this.setState({count:this.state.count+1})
+  
+  this.setState({count:this.state.count+1})
+  this.c.push(this.state.count)
+  this.setState({cards:this.c})
+  
+  //alert(this.state.cards)
+}
+DeleteButtonHandler=(x)=> {
+  
+  const notes = Object.assign([],this.state.cards)
+  notes.splice(x,1)
+  this.setState({cards:notes})
+
+  alert(x)  
+}
+
+
   render() {
+      const children =[]
+      //const cards=[]
+      // for(let i=0;i<this.state.cards;i++){
+
+      // children.push(<p  key={i}>
+      //                   <input  class="box"/>{i}
+      //                   <button onClick={this.AddButtonHandler}>add</button>
+      //                   <button onClick={this.DeleteButtonHandler}>del</button>        
+      //                                 </p>)
+      // }
+
+        this.state.cards.map(x =>  children.push(<p>
+        <input  class="box"/>{x}
+        <button onClick={this.AddButtonHandler}>add</button>
+        <button onClick={this.DeleteButtonHandler.bind(this,x)}>del</button>        
+                      </p>)                
+        )
+
+
+
     return (
       <div>
-        <input   class="box"/><button >add</button>
+        <div>
+        <input   class="box"/>
+        <button onClick={this.AddButtonHandler}>add</button>
+        <button onClick={this.DeleteButtonHanlder}>del</button>
+        </div>
+        {children}
       </div>
+
+     
     )
   }
 }
